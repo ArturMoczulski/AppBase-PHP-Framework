883507c059f8f99f4b853257cf960984bf207828
Artur Moczulski <artur.moczulski@gmail.com>
2013-08-14 22:13:52 -0700

    Helper methods for rendering action links.

--- classes/Core/Helpers/HTML.class.php
+++ classes/Core/Helpers/HTML.class.php
@@ -3,6 +3,44 @@ namespace Core\Helpers;
 
 class HTML extends Helper {
 
+  public function action($aAction, $aAttributes=array()) {
+    $sOutput = "";
+
+    if( !isset($aAction['sLinkName'])) $aAction['sLinkName'] = ucfirst($aAction['sActionName']);
+
+    if( isset($aAction['sLinkName']) && isset($aAction['sControllerName']) && isset($aAction['sActionName'])) {
+      $sOutput .= $this->link($aAction['sLinkName'], $aAction['sControllerName'], $aAction['sActionName'], isset($aAction['aArguments']) ? $aAction['aArguments'] : array(), "", "btn");
+    }
+    return $sOutput;
+  }
+
+  public function actions($aActions, $aAttributes=array()) {
+    $sOutput = ' <div class="btn-group">';
+    foreach( $aActions as $aAction ) {
+      $sOutput .= $this->action($aAction);
+    }
+    $sOutput .= '              </div> ';
+    return $sOutput;
+  }
+
+  public function actionsMenu($aActions, $aAttributes=array()) {
+    $sOutput = '
+                    <div class="well" style="padding: 8px 0;">
+                        <ul class="nav nav-list">
+                            <li class="nav-header">Actions</li>';
+    foreach( $aActions as $aAction ) {
+      if( isset($aAction['sLinkName']) && isset($aAction['sControllerName']) && isset($aAction['sActionName'])) {
+        $sOutput .= '<li>';
+        $sOutput .= $this->link($aAction['sLinkName'], $aAction['sControllerName'], $aAction['sActionName'], isset($aAction['aArguments']) ? $aAction['aArguments'] : array());
+        $sOutput .= '</li>';
+      }
+    }
+    $sOutput .= '              </ul>
+                    </div>
+      ';
+    return $sOutput;
+  }
+
   public function tag($sTagName, $sContent, $sId="", $sClassNames="", $aAttributes=array()) {
     $sAttributes = "";
     foreach( $aAttributes as $sAttributeName => $sAttributeValue) {
